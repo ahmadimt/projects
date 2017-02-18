9807c775d31462f1682e1ae7c47f8c4e2daadd06
Imteyaz Ahmad <ahmad.imt07@gmail.com>
2017-02-18 15:49:07 +0530

    Changed test packages name

--- src/test/java/com/imti/ApplicationTests.java
+++ src/test/java/com/imti/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.imti.techolution;
+package com.imti;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TecholutionApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
